a9e5d7303cdb0af6ce3dc0c865b1f1a27615f935
xixvork <154737979+xixvork@users.noreply.github.com>
2026-03-05 19:27:51 +0200

    made UniversityRegistry.cs

--- Polymorphism/UniversityRegistry.cs
+++ Polymorphism/UniversityRegistry.cs
@@ -1,6 +1,37 @@
 namespace Polymorphism;
 
+using System;
+using System.Collections.Generic;
+
 public class UniversityRegistry
 {
-    
+    private readonly List<UniversityMember> members = new List<UniversityMember>();
+
+    public void AddMember(UniversityMember m)
+    {
+        if (m is null)
+            throw new ArgumentNullException(nameof(m));
+
+        members.Add(m);
+    }
+
+    public void ExecuteAllDuties()
+    {
+        foreach (var m in members)
+        {
+            m.PerformDuties();
+        }
+    }
+
+    public int GetMemberStatistics()
+    {
+        int total = 0;
+
+        foreach (var m in members)
+        {
+            total += m.TotalActions; 
+        }
+
+        return total;
+    }
 }
