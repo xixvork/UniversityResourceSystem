02f39a91006ededf1a9c6620bfbe0f7c0a5d9c71
xixvork <154737979+xixvork@users.noreply.github.com>
2026-03-05 19:21:20 +0200

    made UniversityMember.cs

--- Polymorphism/UndergraduateStudent.cs
+++ Polymorphism/UndergraduateStudent.cs
@@ -1,6 +1,12 @@
 namespace Polymorphism;
 
-public class UndergraduateStudent
+public class UndergraduateStudent : UniversityMember
 {
-    
+    public UndergraduateStudent(string name, string memberId) : base(name, memberId) {}
+
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        ActionLog.Add("Lab work completed");
+    }
 }
