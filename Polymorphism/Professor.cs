06652b6873c4171b9ae37e94cea128fae9a73be2
xixvork <154737979+xixvork@users.noreply.github.com>
2026-03-05 19:13:42 +0200

    made Professor.cs

--- Polymorphism/Professor.cs
+++ Polymorphism/Professor.cs
@@ -1,6 +1,21 @@
 namespace Polymorphism;
 
-public class Professor
+public class Professor : UniversityMember
 {
-    
+    public Professor(string name, string memberId) :  base(name, memberId) {}
+
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        ActionLog.Add("Lecture delivered");
+    }
+
+    public void ConductResearch(string topic)
+    {
+        if (string.IsNullOrWhiteSpace(topic))
+            throw new ArgumentException("Topic cannot be empty.", nameof(topic));
+
+        base.PerformDuties();
+        ActionLog.Add($"Research conducted: {topic}");
+    }
 }
