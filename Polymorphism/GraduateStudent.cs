338e6cf70da189086ca58759f607b284f571cc77
xixvork <154737979+xixvork@users.noreply.github.com>
2026-03-05 19:23:13 +0200

    made GraduateStudent.cs

--- Polymorphism/GraduateStudent.cs
+++ Polymorphism/GraduateStudent.cs
@@ -1,6 +1,12 @@
 namespace Polymorphism;
 
-public class GraduateStudent
+public class GraduateStudent : UndergraduateStudent
 {
-    
+    public GraduateStudent(string name, string memberId) : base(name, memberId) { }
+
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        ActionLog.Add("Thesis research update");
+    }
 }
